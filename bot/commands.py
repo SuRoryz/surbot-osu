0418dc2bc1f7cc8c2371223f38f68663e5d23bd3
SuRoryz <64648463+SuRoryz@users.noreply.github.com>
2020-05-04 23:34:09 +0300

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -272,7 +272,7 @@ def calcPred(*args) -> str:
 
 # INFO
 def info(*args):
-    mess = 'You can check commands info [https://pastebin.com/ZEBPuxix here]'
+    mess = 'You can check commands info [https://pastebin.com/ZEBPuxix here]. [https://suroryz.github.io/surbot_osu/ Source & contact]'
 
     return mess
 
